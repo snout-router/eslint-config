b51a433b328b90dd3f4d5e20661d31df3a595d28
Erin Millard <ezzatron@gmail.com>
2021-08-16 21:46:32 +1000

    Add support for React

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   extends: [
+    'standard-jsx',
+    'standard-react',
     'standard-with-typescript',
   ],
   parserOptions: {
@@ -14,6 +16,12 @@ module.exports = {
     'object-curly-spacing': ['error', 'never'],
     // makes it harder to overlook early return statements
     'padding-line-between-statements': ['error', {blankLine: 'always', prev: '*', next: 'return'}],
+    // no requirement for parens around multi-line JSX
+    'react/jsx-wrap-multilines': 'off',
+    // broken with react/jsx-wrap-multilines disabled, until https://github.com/yannickcr/eslint-plugin-react/issues/1466 is addressed
+    'react/jsx-closing-tag-location': 'off',
+    // no requirement for prop types
+    'react/prop-types': 'off',
   },
   overrides: [
     {
