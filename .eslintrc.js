d4d47c50628840340b755355403fd11374d236ad
Erin Millard <ezzatron@gmail.com>
2021-08-17 20:01:41 +1000

    Remove React config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   extends: [
-    'standard-jsx',
-    'standard-react',
     'standard-with-typescript',
   ],
   parserOptions: {
@@ -16,12 +14,6 @@ module.exports = {
     'object-curly-spacing': ['error', 'never'],
     // makes it harder to overlook early return statements
     'padding-line-between-statements': ['error', {blankLine: 'always', prev: '*', next: 'return'}],
-    // no requirement for parens around multi-line JSX
-    'react/jsx-wrap-multilines': 'off',
-    // broken with react/jsx-wrap-multilines disabled, until https://github.com/yannickcr/eslint-plugin-react/issues/1466 is addressed
-    'react/jsx-closing-tag-location': 'off',
-    // no requirement for prop types
-    'react/prop-types': 'off',
   },
   overrides: [
     {
